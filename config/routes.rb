0559c20e0089984b01d5e5ba02a1f6528029a038
Rory Gianni <giannichan@gmail.com>
2013-09-05 13:47:56 +0100

    added root route and a route to latest stations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Firstfm::Application.routes.draw do
   resources :stations
+  get 'latest' => 'stations#latest'
 
 
   # The priority is based upon order of creation:
@@ -51,7 +52,7 @@ Firstfm::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'stations#index'
 
   # See how all your routes lay out with "rake routes"
 
