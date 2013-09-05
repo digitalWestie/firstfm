036d15fefe8a289e6d9bdb3beafded464a40ce0c
Rory Gianni <giannichan@gmail.com>
2013-09-05 17:43:02 +0100

    added favourite and unfavourite links, just use notice for basic example

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -5,14 +5,14 @@ class FavouritesController < ApplicationController
   def favourite
     @station = Station.find(params[:id])
     current_user.stations << @station
-    render :text => 'ok'
+    redirect_to stations_path, :notice => "#{@station.name} added to favourites"
   end
 
   def unfavourite
-    @station = Station.find()
+    @station = Station.find(params[:id])
     fav = current_user.favourites.find_by_station_id(params[:id])
     fav.destroy
-    render :text => 'ok'
+    redirect_to stations_path, :notice => "#{@station.name} removed from favourites"
   end
 
 end
