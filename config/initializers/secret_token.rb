e9b6a2af73f452ee5b18cb016d83528d34392858
Rory Gianni <giannichan@gmail.com>
2013-09-05 11:41:47 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstfm::Application.config.secret_token = '7005843edbc55b0a0eb53543792d8001ec2a2ce0a08cdf2c8699d0e2e562bd288e3759a4d7ab2341b58b08830983dc1d36f6d1e953c9e01716646b0e501466ea'
